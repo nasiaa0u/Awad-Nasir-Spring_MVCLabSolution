c63c84009bd5734ec60ec747f30f137728a804b0
nasiaa0u <93525279+nasiaa0u@users.noreply.github.com>
2022-03-18 04:50:31 -0700

    Add files via upload

---
+++ StudentService.java
@@ -0,0 +1,19 @@
+package com.greatlearning.model;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+public interface StudentService {
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+	//public List<Student> searchBy(String name, String author);
+
+}
